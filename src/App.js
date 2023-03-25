96b320247209528f881b848c646d33d29b235620
ronak-d <ronakdodeja73@gamil.com>
2023-03-25 21:52:21 +0530

    resolved errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import AllRoutes from "./Routers/AllRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App"></div>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AllRoutes />
+        <Navbar />
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Navbar = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+          <Link class="navbar-brand" to={"/home"}>
             Hoyo
-          </a>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Hotels
-                </a>
-              </li>
-            </ul>
+          </Link>
+          <Link class="navbar-brand" to={"/hotels"}>
+            Hotels
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            // data-bs-toggle="collapse"
+            // data-bs-target="#navbarSupportedContent"
+            // aria-controls="navbarSupportedContent"
+            // aria-expanded="false"
+            // aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search for Hotels"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
           </div>
         </div>
       </nav>
